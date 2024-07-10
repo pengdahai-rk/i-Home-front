b2342dcc9eaeaf2369cef8478e8c58eb96b89daf
pengdahai <pengdahai216@126.com>
2024-07-10 14:22:08 +0800

    引入element-plus

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,20 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueDevTools from 'vite-plugin-vue-devtools'
 import fs from 'node:fs'
 import path from 'node:path'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), VueDevTools()],
+  plugins: [vue(), vueJsx(), VueDevTools(),
+    AutoImport({
+      resolvers:[ElementPlusResolver()],
+    }),
+    Components({
+      resolvers:[ElementPlusResolver()],
+    })
+  ],
   resolve: {
     alias: {
       '@': path.join(__dirname, 'src')
