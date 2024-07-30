d698bb8a25080c0da2f264fdd25398ad46179fe3
pengdahai <pengdahai216@126.com>
2024-07-30 17:31:29 +0800

    项目重构

--- components.d.ts
+++ components.d.ts
@@ -9,6 +9,10 @@ declare module 'vue' {
   export interface GlobalComponents {
     BaseHeader: typeof import('./src/layout/components/BaseHeader.vue')['default']
     DemoComponent: typeof import('./src/components/DemoComponent.vue')['default']
+    ElImg: typeof import('element-plus/es')['ElImg']
+    ElMenu: typeof import('element-plus/es')['ElMenu']
+    ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
+    ElSubMenu: typeof import('element-plus/es')['ElSubMenu']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
--- src/layout/components/BaseHeader.vue
+++ src/layout/components/BaseHeader.vue
@@ -1,27 +1,43 @@
 <template>
     <header class="navbar">
-        <div class="navbar-wrapper">
-            <div class="header-container">
-                <div class="logo-container">
-                    <a href="/">
-                        <img class="logo" src="@/assets/logo.svg" alt="Element Plus Logo" />
-                    </a>
-                </div>
-                <div class="content">
-
-                    <div class="social-links">
-                        <!--[--><a href="https://github.com/pengdahai-rk/i-Home-front" title="GitHub" target="_blank"
-                            rel="noreferrer noopener" class="social-link"><i class="el-icon"
-                                style="font-size: 24px"><!--[--><svg preserveAspectRatio="xMidYMid meet"
-                                    viewBox="0 0 24 24" width="1.2em" height="1.2em">
-                                    <path fill="currentColor"
-                                        d="M12 2C6.475 2 2 6.475 2 12a9.994 9.994 0 0 0 6.838 9.488c.5.087.687-.213.687-.476c0-.237-.013-1.024-.013-1.862c-2.512.463-3.162-.612-3.362-1.175c-.113-.288-.6-1.175-1.025-1.413c-.35-.187-.85-.65-.013-.662c.788-.013 1.35.725 1.538 1.025c.9 1.512 2.338 1.087 2.912.825c.088-.65.35-1.087.638-1.337c-2.225-.25-4.55-1.113-4.55-4.938c0-1.088.387-1.987 1.025-2.688c-.1-.25-.45-1.275.1-2.65c0 0 .837-.262 2.75 1.026a9.28 9.28 0 0 1 2.5-.338c.85 0 1.7.112 2.5.337c1.912-1.3 2.75-1.024 2.75-1.024c.55 1.375.2 2.4.1 2.65c.637.7 1.025 1.587 1.025 2.687c0 3.838-2.337 4.688-4.562 4.938c.362.312.675.912.675 1.85c0 1.337-.013 2.412-.013 2.75c0 .262.188.574.688.474A10.016 10.016 0 0 0 22 12c0-5.525-4.475-10-10-10z">
-                                    </path>
-                                </svg><!--]--></i></a><!--]-->
-                    </div>
-                </div>
-            </div>
-        </div>
+        <el-menu  :ellipsis="false" mode="horizontal" class="navbar-el-menu">
+            <el-menu-item index="1">
+                <a href="/">
+                    <img class="logo" src="@/assets/logo.svg" alt="Element Plus Logo" />
+                </a>
+            </el-menu-item>
+            <el-sub-menu index="2">
+                <template #title>Workspace</template>
+                <el-menu-item index="2-1">item one</el-menu-item>
+                <el-menu-item index="2-2">item two</el-menu-item>
+                <el-menu-item index="2-3">item three</el-menu-item>
+                <el-sub-menu index="2-4">
+                    <template #title>item four</template>
+                    <el-menu-item index="2-4-1">item one</el-menu-item>
+                    <el-menu-item index="2-4-2">item two</el-menu-item>
+                    <el-menu-item index="2-4-3">item three</el-menu-item>
+                </el-sub-menu>
+            </el-sub-menu>
+            <el-menu-item index="3" disabled>Info</el-menu-item>
+            <el-menu-item index="4">Orders</el-menu-item>
+            <el-menu-item h="full">
+                <button class="border-none w-full bg-transparent cursor-pointer"
+                    style="height: var(--ep-menu-item-height)">
+                    <i inline-flex i="dark:ep-moon ep-sunny" />
+                </button>
+            </el-menu-item>
+            <div class="flex-grow" />
+            <el-menu-item index="5">Orders</el-menu-item>
+            <el-menu-item index = "6">
+                <a href="https://github.com/pengdahai-rk/i-Home-front" title="GitHub" target="_blank"
+                    rel="noreferrer noopener" class="social-link"><i class="el-icon" style="font-size: 24px"><svg
+                            preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24" width="1.2em" height="1.2em">
+                            <path fill="currentColor"
+                                d="M12 2C6.475 2 2 6.475 2 12a9.994 9.994 0 0 0 6.838 9.488c.5.087.687-.213.687-.476c0-.237-.013-1.024-.013-1.862c-2.512.463-3.162-.612-3.362-1.175c-.113-.288-.6-1.175-1.025-1.413c-.35-.187-.85-.65-.013-.662c.788-.013 1.35.725 1.538 1.025c.9 1.512 2.338 1.087 2.912.825c.088-.65.35-1.087.638-1.337c-2.225-.25-4.55-1.113-4.55-4.938c0-1.088.387-1.987 1.025-2.688c-.1-.25-.45-1.275.1-2.65c0 0 .837-.262 2.75 1.026a9.28 9.28 0 0 1 2.5-.338c.85 0 1.7.112 2.5.337c1.912-1.3 2.75-1.024 2.75-1.024c.55 1.375.2 2.4.1 2.65c.637.7 1.025 1.587 1.025 2.687c0 3.838-2.337 4.688-4.562 4.938c.362.312.675.912.675 1.85c0 1.337-.013 2.412-.013 2.75c0 .262.188.574.688.474A10.016 10.016 0 0 0 22 12c0-5.525-4.475-10-10-10z">
+                            </path>
+                        </svg></i></a>
+            </el-menu-item>
+        </el-menu>
     </header>
 </template>
 <script setup lang="ts">
@@ -34,49 +50,17 @@
     z-index: var(--nav-z-index);
 }
 
-.navbar-wrapper {
-    position: relative;
-    border-bottom: 1px solid var(--border-color);
+.navbar-el-menu {
     height: var(--header-height);
-    padding: 0 12px 0 24px;
     background-image: radial-gradient(transparent 1px, var(--bg-color) 1px);
-    background-size: 4px 4px;
-    backdrop-filter: saturate(50%) blur(4px);
-    -webkit-backdrop-filter: saturate(50%) blur(4px);
-    top: 0
 }
 
-.navbar-wrapper .header-container {
-    display: flex;
-    justify-content: space-between;
-    margin: 0 auto;
-}
-
-.navbar-wrapper .header-container .content {
-    display: flex;
-    justify-content: flex-end;
-    align-items: center;
+.flex-grow {
     flex-grow: 1;
 }
 
-.logo-container {
-    display: flex;
-    align-items: center;
-    height: var(--header-height);
-}
-
-.logo-container>a {
-    height: 28px;
-    width: 128px;
-}
-
-.logo-container .logo {
+.logo {
     position: relative;
-    height: 100%;
-}
-
-.social-links {
-    height: 24px;
-    padding: 0 12px;
+    height: var(--header-height);
 }
 </style>
